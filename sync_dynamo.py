5827797246df65cec3849f58b9962857e7e70647
bart0709 <bart0709@stud.kea.dk>
2021-11-06 12:11:11 +0100

    added saving from list of links

--- sync_dynamo.py
+++ sync_dynamo.py
@@ -3,7 +3,23 @@ import urllib.request
 import requests as rqs
 from os.path import expanduser
 home = expanduser("~")
+filePath ="{}/Appdata/Roaming/pyRevit-Master/extensions/pyRevitKEA.extensions/DynamoLibrary/".format(home)
+# filePath ="{}/Appdata/Roaming/pyRevit-Master/extensions/pyRevitKEA.extensions/KEA.tab/Libraries.panel/Dynamo.pushbutton/".format(home)
+if not os.path.exists(os.path.dirname(filePath)):
+    try:
+        os.makedirs(os.path.dirname(filePath))
+    except OSError as exc: # Guard against race condition
+        if exc.errno != errno.EEXIST:
+            raise
 
-# file = urllib.request.urlretrieve(url, savePath)
+url = open("scriptlist", "r").readlines()
+url = [i.split("\n")[0] for i in url]
+
+for path in url:
+    fileName = path.split('/')[-1]
+    fileName = filePath + fileName
+    file = urllib.request.urlretrieve(path, fileName)
+
+
 
 
