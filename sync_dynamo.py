531598907bc9e1d0cba4303eb11bed51551a907c
bart0709 <bart0709@stud.kea.dk>
2021-11-06 11:15:10 +0100

    fixed home-folder-finding

--- sync_dynamo.py
+++ sync_dynamo.py
@@ -1,9 +1,9 @@
 import os
 import urllib.request
 import requests as rqs
-url = ""
-savePath = os.path.expanduser
-file = urllib.request.urlretrieve(url, savePath)
-file = open(savePath+"/test.txt","w")
+from os.path import expanduser
+home = expanduser("~")
+
+# file = urllib.request.urlretrieve(url, savePath)
 
 
