4d862cea2550ae93edf8b377ffb89543aaf5e46a
bart0709 <bart0709@stud.kea.dk>
2021-11-06 21:15:16 +0100

    very small fixes

--- screenshotScript.py
+++ screenshotScript.py
@@ -21,13 +21,14 @@ def uploadImage(username, filepath):
         }
     response = rqs.request("POST", url, headers = headers, data=payload)
     # response = """{"status":200,"success":true,"data":{"id":"UiT0D83","deletehash":"pFTC4iBAhLO6XPM","account_id":null,"account_url":null,"ad_type":null,"ad_url":null,"title":"Screenshot of the dummy","description":null,"name":"test.png","type":"image/png","width":360,"height":360,"size":30493,"views":0,"section":null,"vote":null,"bandwidth":0,"animated":false,"favorite":false,"in_gallery":false,"in_most_viral":false,"has_sound":false,"is_ad":false,"nsfw":null,"link":"https://i.imgur.com/UiT0D83.png","tags":[],"datetime":1636226909,"mp4":"","hls":""}}"""
-    return json.loads(response)["data"]["link"]
+    response = json.loads(response.text)
+    return str(response["data"]["link"])
 
 
 
 home = os.path.expanduser("~")
 username = "dummy" # ! change it so it takes it from Revit 
 # filepath = os.path.normpath(home +"/Desktop/{}.png".format(username)) # ! turn it on when you want to upload a screen
-# filepath = os.path.normpath(home +"/Desktop/test.png")
+filepath = os.path.normpath(home +"/Desktop/test.png")
 # takeScreenshot(filepath) # ! turn it on later
 print(uploadImage(username, filepath))
