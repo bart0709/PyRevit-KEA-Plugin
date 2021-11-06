fea058f82961ae049ce04280175f9051a346951d
bart0709 <bart0709@stud.kea.dk>
2021-11-06 13:55:29 +0100

    added automatic button creation

--- sync_dynamo.py
+++ sync_dynamo.py
@@ -3,22 +3,26 @@ import urllib.request
 import requests as rqs
 from os.path import expanduser
 home = expanduser("~")
-filePath ="{}/Appdata/Roaming/pyRevit-Master/extensions/pyRevitKEA.extensions/DynamoLibrary/".format(home)
-# filePath ="{}/Appdata/Roaming/pyRevit-Master/extensions/pyRevitKEA.extensions/KEA.tab/Libraries.panel/Dynamo.pushbutton/".format(home)
-if not os.path.exists(os.path.dirname(filePath)):
-    try:
-        os.makedirs(os.path.dirname(filePath))
-    except OSError as exc: # Guard against race condition
-        if exc.errno != errno.EEXIST:
-            raise
 
 url = open("scriptlist", "r").readlines()
 url = [i.split("\n")[0] for i in url]
 
-for path in url:
-    fileName = path.split('/')[-1]
-    fileName = filePath + fileName
-    file = urllib.request.urlretrieve(path, fileName)
+# filePath ="{}/Appdata/Roaming/pyRevit-Master/extensions/pyRevitKEA.extensions/DynamoLibrary/".format(home)
+filePath ="{}/Appdata/Roaming/pyRevit-Master/extensions/pyRevitKEA.extensions/KEA.tab/Libraries.panel/Dynamo Player.pulldown/".format(home)
+
+
+for link in url:
+    fileName = link.split('/')[-1] 
+    fileName = filePath + "/" + fileName.split(".")[0]+ ".extensions/"+ fileName
+
+    if not os.path.exists(os.path.dirname(fileName)):
+        try:
+            os.makedirs(os.path.dirname(fileName))
+        except OSError as exc: # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+    file = urllib.request.urlretrieve(link, fileName)
+
 
 
 
