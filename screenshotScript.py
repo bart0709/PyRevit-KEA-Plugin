a3661603fac0b3eab77505879ee318711c37c09b
bart0709 <90346110+bart0709@users.noreply.github.com>
2021-11-07 11:33:10 +0100

    Update screenshotScript.py

--- screenshotScript.py
+++ screenshotScript.py
@@ -3,6 +3,7 @@ import requests as rqs
 import os.path 
 from base64 import b64encode
 import json
+import scriptSQL
 
 def takeScreenshot(filepath):
     screenshot  = ImageGrab.grab()
@@ -22,13 +23,17 @@ def uploadImage(username, filepath):
     response = rqs.request("POST", url, headers = headers, data=payload)
     # response = """{"status":200,"success":true,"data":{"id":"UiT0D83","deletehash":"pFTC4iBAhLO6XPM","account_id":null,"account_url":null,"ad_type":null,"ad_url":null,"title":"Screenshot of the dummy","description":null,"name":"test.png","type":"image/png","width":360,"height":360,"size":30493,"views":0,"section":null,"vote":null,"bandwidth":0,"animated":false,"favorite":false,"in_gallery":false,"in_most_viral":false,"has_sound":false,"is_ad":false,"nsfw":null,"link":"https://i.imgur.com/UiT0D83.png","tags":[],"datetime":1636226909,"mp4":"","hls":""}}"""
     response = json.loads(response.text)
-    return str(response["data"]["link"])
+    link = str(response["data"]["link"])
+    # print(link)
+    return link
 
 
 
 home = os.path.expanduser("~")
 username = "dummy" # ! change it so it takes it from Revit 
-# filepath = os.path.normpath(home +"/Desktop/{}.png".format(username)) # ! turn it on when you want to upload a screen
-filepath = os.path.normpath(home +"/Desktop/test.png")
-# takeScreenshot(filepath) # ! turn it on later
-print(uploadImage(username, filepath))
+filepath = os.path.normpath(home +"/Desktop/{}.png".format(username)) # ! turn it on when you want to upload a screen
+# filepath = os.path.normpath(home +"/Desktop/test.png") 
+takeScreenshot(filepath) # ! turn it on later
+link = uploadImage(username, filepath)
+db = scriptSQL.Database()
+db.insert("names, links", link)
